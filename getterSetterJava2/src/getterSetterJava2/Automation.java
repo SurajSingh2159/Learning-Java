fc53f70b2eaf79f7f6449f70a0be8238e22537b7
Suraj Rezolut <surajkumarsingh2159@gmail.com>
2020-03-26 17:38:28 +0530

    Automating getter setter

---
+++ getterSetterJava2/src/getterSetterJava2/Automation.java
@@ -0,0 +1,57 @@
+//Creating setters and getters automatically using insert code facility
+package getterSetterJava2;
+
+class Products
+{
+long pcode;
+float price;
+// How we will create getter setter without having type the code
+public long getPcode() //alt+shift+S
+{
+	return pcode;
+}
+public void setPcode(long pcode) 
+{
+	this.pcode = pcode;
+}
+public float getPrice() 
+{
+	return price;
+}
+public void setPrice(float price) 
+{
+	this.price = price;
+}
+
+/*void setPcode(long pcode)// setter for pcode
+{
+this.pcode=pcode;	
+}
+long getPcode()/getter for pcode
+{
+ return pcode;	
+}
+void setPrice(float price)//setter for price
+{
+this.price=price;	
+}
+float getPrice()//getter for price
+{
+return price;	
+}*/
+}
+public class Automation {
+
+	public static void main(String[] args) {
+		Products pr1= new Products();
+		pr1.setPcode(10001);
+		long mpcode=pr1.getPcode();
+		System.out.println("Product code="+mpcode);
+		pr1.setPrice(1000100);
+		float mprice=pr1.getPrice();
+		System.out.println("Product price="+mprice);
+		
+
+	}
+
+}
