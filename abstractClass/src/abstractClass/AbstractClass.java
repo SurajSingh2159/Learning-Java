44cbea229fb9bb1cce966501ce7ee73ee16c48e9
Suraj Rezolut <surajkumarsingh2159@gmail.com>
2020-04-17 15:10:13 +0530

    object cannot be created in abstract class

---
+++ abstractClass/src/abstractClass/AbstractClass.java
@@ -0,0 +1,16 @@
+package abstractClass;
+abstract class Person
+{
+int age;
+float salary;
+}
+
+public class AbstractClass {
+
+	public static void main(String[] args) {
+	Person pref=new Person();// object cannnot be instantiated in abstract classes
+	Person pref;// mere declaration of a reference variable of the abstract class
+
+	}
+
+}
