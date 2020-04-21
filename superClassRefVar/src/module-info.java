4f7dcfeb016bf832abef2af3c8aafd29e0327555
Suraj Rezolut <surajkumarsingh2159@gmail.com>
2020-04-21 12:57:55 +0530

    Ref variable of super class can contain the ref of sub class object

---
+++ superClassRefVar/src/module-info.java
@@ -0,0 +1,2 @@
+module superClassRefVar {
+}
---
+++ superClassRefVar/src/superClassRefVar/SuperClassRefVar.java
@@ -0,0 +1,10 @@
+package superClassRefVar;
+
+public class SuperClassRefVar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
