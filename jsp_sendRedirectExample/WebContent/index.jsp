16afa42e7efe28d8a293be7439205f906c7d1aec
Suraj Rezolut <surajkumarsingh2159@gmail.com>
2020-06-17 19:08:20 +0530

    Jsp Send Redirect

---
+++ jsp_sendRedirectExample/WebContent/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1><center>Credential Form</center></h1>
+<form action="process" method="post">
+Enter Username: <input type="text" name="uname">
+Enter Password: <input type="password" name="pass">
+<input type="submit" value="Check Credentials">
+</form>
+</body>
+</html>
---
+++ jsp_sendRedirectExample/src/suraj/process.java
@@ -0,0 +1,48 @@
+package suraj;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class process
+ */
+@WebServlet("/process")
+public class process extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public process() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		response.setContentType("text/html");
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		if(uname.equals("admin") && pass.equals("admin"))
+			response.sendRedirect("sucess");
+		else
+			response.sendRedirect("failure");
+	}
+
+}
