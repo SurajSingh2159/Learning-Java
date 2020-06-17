9c427a1a4f87d999d7eaaf7239fb1477407d10f8
Suraj Rezolut <surajkumarsingh2159@gmail.com>
2020-06-17 19:10:41 +0530

    Jsp Servlet config Parameter

---
+++ jsp_servlet_Config_Parameter/src/suraj/myservlet.java
@@ -0,0 +1,49 @@
+package suraj;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class myservlet
+ */
+//@WebServlet("/myservlet")
+public class myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		ServletConfig convar=getServletConfig();
+		String str=convar.getInitParameter("amount");// here amount is a config parameter in web.xml
+		out.print("<h1>Amount="+str+"</h1>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
