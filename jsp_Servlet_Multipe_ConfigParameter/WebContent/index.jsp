d5db22d1eb462074586a1c2f373eae135ec82beb
Suraj Rezolut <surajkumarsingh2159@gmail.com>
2020-06-17 19:04:09 +0530

    Jsp servlet multiple config parameter

---
+++ jsp_Servlet_Multipe_ConfigParameter/WebContent/index.jsp
@@ -0,0 +1,1 @@
+<a href="myservlet">Click Here</a>
---
+++ jsp_Servlet_Multipe_ConfigParameter/src/suraj/myservlet.java
@@ -0,0 +1,56 @@
+package suraj;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class myservlet
+ */
+//@WebServlet("/myservlet")
+public class myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		ServletConfig cv=getServletConfig();
+		Enumeration <String> evar=cv.getInitParameterNames();
+		String str="";
+		while(evar.hasMoreElements())
+		{
+			str=evar.nextElement();
+			out.print("<br>"+cv.getInitParameter(str));
+		}
+		out.close();
+		}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
