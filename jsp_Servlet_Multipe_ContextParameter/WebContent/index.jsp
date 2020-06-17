6de7f3cbbdfb5b4566b3d1a0d5e8b4fb253362f6
Suraj Rezolut <surajkumarsingh2159@gmail.com>
2020-06-17 19:05:44 +0530

    Jsp servlet multiple context parameter

---
+++ jsp_Servlet_Multipe_ContextParameter/WebContent/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h1>This is an example where we use Enumeration</h1>
+<h2>A loop will be applied to pick up context parameter one by one</h2>
+<form action="myservlet" method="post">
+<input type="submit" value="click me">
+</form>
+</body>
+</html>
---
+++ jsp_Servlet_Multipe_ContextParameter/src/suraj/myservlet.java
@@ -0,0 +1,56 @@
+package suraj;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class myservlet
+ */
+@WebServlet("/myservlet")
+public class myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	response.setContentType("text/html");
+	PrintWriter out=response.getWriter();
+	ServletContext ctx=getServletContext();
+	Enumeration <String> ever=ctx.getInitParameterNames();
+	String str="";
+	while(ever.hasMoreElements())
+	{
+		str=ever.nextElement();
+		out.print("<br>"+ctx.getInitParameter(str));
+	}
+	out.close();
+	}
+
+}
